e567915873c63ca89cbcfbf93783e76f672e92a0
Jona-David Bastian <jona-david.bastian@mni.thm.de>
2025-07-21 22:13:34 +0200

    Add: Initial commit, add Java Spring core structure.

---
+++ src/main/java/com/jabamo/NuxtLightweightCms/NuxtLightweightCmsApplication.java
@@ -0,0 +1,13 @@
+package com.jabamo.NuxtLightweightCms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuxtLightweightCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuxtLightweightCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabamo/NuxtLightweightCms/NuxtLightweightCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabamo.NuxtLightweightCms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuxtLightweightCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
